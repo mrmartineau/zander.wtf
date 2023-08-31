13bad72c73c945ba8f69419783e254bd72e7f0fe
Zander Martineau <mrmartineau@users.noreply.github.com>
2023-08-31 07:14:44 +0100

    Improve syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,19 @@ const commitHash = execSync('git rev-parse --short HEAD').toString()
 export default defineConfig({
   site: 'https://zander.wtf',
   integrations: [mdx(), sitemap(), prefetch()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'poimandres',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
   vite: {
     define: {
       __COMMIT_HASH__: JSON.stringify(commitHash),
